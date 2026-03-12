73f79a44c8a942ee4f9b6ce6d1746285534e83a4
MapateLoum <loumpapamapate@gmail.com>
2026-03-12 21:16:12 +0000

    Modif du main pour autorisation.

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
+import ssl
 
 load_dotenv()
 
-client = AsyncIOMotorClient(os.getenv("MONGO_URL"))
+client = AsyncIOMotorClient(
+    os.getenv("MONGO_URL"),
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
 db = client[os.getenv("DB_NAME")]
